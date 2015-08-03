f5aa5179905e22489f04e41d22bb57520400c648
Mariusz <camusv@gmail.com>
2015-08-03 21:00:10 +0200

    update

--- Training_sessions/commands/commands/echo.py
+++
@@ -1,9 +0,0 @@
-from commands import Command
-
-class Echo(Command):
-    cmd_name = 'echo'
-    cmd_description = 'Echo "Hello, World!!!"'
-
-    def execute(self):
-        self._run(['echo', 'Hello, World!!!'])
-
